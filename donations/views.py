d4ea9750f09976339aeea48f3286e46304566062
NestaNdereva23 <nestandereva@gmail.com>
2024-12-18 09:58:15 +0300

    homepage update

--- donations/views.py
+++ donations/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'donations/index.html')
+
+def donation_page(request):
+    return render(request, 'donations/donation.html')
